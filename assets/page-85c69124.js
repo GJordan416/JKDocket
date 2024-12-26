d5a5bfa31041142636bf9f156f8a2ad4a6904bc1
GJordan416 <167732049+GJordan416@users.noreply.github.com>
2024-12-26 08:17:05 -0600

    Create page-85c69124.js

---
+++ assets/page-85c69124.js
@@ -0,0 +1,8 @@
+import {b as o} from "./index-c903cc1f.js";
+const i = o()
+  , e = {
+    mounted() {
+        i.initialPageLoad || this.$cove.focusElement(".page-focus")
+    }
+};
+export {e as P};
